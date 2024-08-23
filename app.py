48d9cef11db85f258291d7d9be96812022c60696
Francis Subasinghe <60639488+Francis-Subasinghe@users.noreply.github.com>
2024-08-23 21:29:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 st.set_page_config(layout="wide")
 
 # Load the saved Ridge Regression model
-model = joblib.load('AutoML_model.pkl')
+model = joblib.load('Stacking_model.pkl')
 
 # Function to make predictions
 def predict(features):
